91b8a673fd86df2acf2e1bc3e534cb8ff652c638
Buhii <buithanhhieubuhi@gmail.com>
2023-03-18 17:17:03 +0700

    Update config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
+import express from "express"
+import configviewEngine from "./configs/viewEngine"
 const app = express()
 const port = 3000
-
+configviewEngine(app);
 app.get('/', (req, res) => {
     res.send('<h1>Bui thanh Hieu<\h1>')
 })
